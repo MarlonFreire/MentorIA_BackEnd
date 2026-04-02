25fed2388d77cfbcc7c44d91bf41ad635b782d8d
MarlonFreire <marlon.freire08@gmail.com>
2026-04-02 19:47:51 -0300

    Setup do server
    setup do serves configurado

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,61 @@
+import dotenv from 'dotenv';
+dotenv.config();
 
+import express from 'express';
+import cors  from 'cors';
+import path  from "path";
+import { fileURLToPath } from "url";
+
+// ESmodule __dirname alternative
+const  __fileName = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__fileName)
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+// middlaware to handle cors
+app.use(
+    cors({
+        origin: "*",
+        methods: ['GET', 'POST','PUT','DELETE'],
+        allowedHeaders: ['Content-Type','Authorization'],
+        credentials:true
+    }))
+
+// Static folder for uploads
+app.use('uploads', express.static(path.join(__dirname, 'uploads')))
+
+// ROTAS
+
+// 404 handler
+app.use( (req,res) =>{
+    res.status(404).json({
+        success: false,
+        error: 'route not found',
+        statuscode: 404
+    })
+})
+
+// start server
+app.listen(3000, () => {
+    console.log('server Rodando')
+})
+
+// Erros assincronos
+process.on('unhandledRejection', (erro) => {
+  if (erro instanceof Error) {
+    console.error(`Error: ${erro.message}`);
+  } else {
+    console.error('Erro desconhecido:', erro);
+  }
+
+  process.exit(1);
+});
+
+// Erros sincronos
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION 💥');
+  console.error(err.stack);
+  process.exit(1);
+});
